35b83fc964c8a771705374c9af6cdda1f164b6c7
ocai0 <caioalvesfernandes@gmail.com>
2026-03-29 00:54:44 -0300

    Adicionado imagens para renderizar as frutas

--- src/Apple.java
+++ src/Apple.java
@@ -1,15 +1,33 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.io.File;
 
 public class Apple extends Fruit {
+    protected BufferedImage sprite;
 
     public Apple(int x, int y) {
         super(x, y, 10);
+        try {
+            this.sprite = ImageIO.read(new File("./sprites/maca.png"));
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void draw(Graphics g) {
-        g.setColor(Color.RED);
-        g.fillOval(x * GamePanel.UNIT_SIZE, y * GamePanel.UNIT_SIZE, GamePanel.UNIT_SIZE, GamePanel.UNIT_SIZE);
+        int uX = x * GamePanel.UNIT_SIZE;
+        int uY = y * GamePanel.UNIT_SIZE;
+        if(this.sprite == null) {
+            g.setColor(Color.RED);
+            g.fillOval(uX, uY, GamePanel.UNIT_SIZE, GamePanel.UNIT_SIZE);
+        }
+        else {
+            g.drawImage(this.sprite, uX, uY, GamePanel.UNIT_SIZE, GamePanel.UNIT_SIZE, null);
+        }
     }
 
     @Override
