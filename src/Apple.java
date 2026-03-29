b110f47df24f7cd42327d032ea8b542878f5e8ab
ocai0 <caioalvesfernandes@gmail.com>
2026-03-29 00:30:13 -0300

    Implementado função Apple.draw

--- src/Apple.java
+++ src/Apple.java
@@ -1,7 +1,15 @@
+import java.awt.*;
+
 public class Apple extends Fruit {
 
     public Apple(int x, int y) {
         super(x, y, 10);
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillOval(x * GamePanel.UNIT_SIZE, y * GamePanel.UNIT_SIZE, GamePanel.UNIT_SIZE, GamePanel.UNIT_SIZE);
     }
 
     @Override
