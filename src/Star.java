36ffde6f2e20f324de20d8bae2356314867322aa
Lucas Ferreira <lucasandradef44@gmail.com>
2026-03-29 22:02:47 -0300

    feat: add boost star para diminuir pela metade o tamanho da cobrinha em determinados momentos do game

---
+++ src/Star.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.io.File;
+
+public class Star extends Boost {
+    protected BufferedImage sprite;
+
+    public Star(int x, int y) {
+        super(x, y);
+        try {
+            this.sprite = ImageIO.read(new File("./sprites/estrela.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        int uX = x * GamePanel.UNIT_SIZE;
+        int uY = y * GamePanel.UNIT_SIZE;
+
+        if (this.sprite == null) {
+            g.setColor(Color.YELLOW);
+            g.fillOval(uX, uY, GamePanel.UNIT_SIZE, GamePanel.UNIT_SIZE);
+        } else {
+            g.drawImage(this.sprite, uX, uY, GamePanel.UNIT_SIZE, GamePanel.UNIT_SIZE, null);
+        }
+    }
+
+    @Override
+    public void applyEffect(GamePanel game) {
+        game.player.halfSize();
+    }
+}
